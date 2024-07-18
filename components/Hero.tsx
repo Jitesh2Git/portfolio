87a4a89f9bd53f97694064c46a801f64d52da521
Jitesh2Git <themastermind2003rv@gmail.com>
2024-07-18 20:26:43 +0530

    resume

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,10 +35,10 @@ const Hero = () => {
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full
          outline-none focus:scale-110 hover:scale-110 active:scale-105 transition 
          cursor-pointer dark:bg-white/10"
-          href="/Jitesh's CV.pdf"
+          href="/Jitesh Mali Resume.pdf"
           target="_blank"
         >
-          Download CV{" "}
+          Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
